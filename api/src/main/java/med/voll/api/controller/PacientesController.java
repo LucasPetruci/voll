4c084c7a1e2108bf905780681a06e82ff0826a2f
Lucas Sales Salvo Petruci <99514230+LucasPetruci@users.noreply.github.com>
2023-05-31 23:11:02 -0300

    Delete, atualizar e detalhar de paciente
    Correção de erro no Banco

--- api/src/main/java/med/voll/api/controller/PacientesController.java
+++ api/src/main/java/med/voll/api/controller/PacientesController.java
@@ -5,8 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +18,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.entity.Paciente;
+import med.voll.api.paciente.DadosAtualizacaoPaciente;
 import med.voll.api.paciente.DadosCadastroPacientes;
 import med.voll.api.paciente.DadosDetalhamentoPaciente;
 import med.voll.api.paciente.DadosListagemPaciente;
@@ -45,5 +49,29 @@ public class PacientesController {
 		return ResponseEntity.ok(page);
 		
 	}
+	
+	@PutMapping
+    @Transactional
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
+        var paciente = repository.getReferenceById(dados.id());
+        paciente.atualizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoPaciente(paciente));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity excluir(@PathVariable Long id) {
+        var paciente = repository.getReferenceById(id);
+        paciente.excluir();
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var paciente = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoPaciente(paciente));
+    }
 
 }
--- api/src/main/java/med/voll/api/entity/Paciente.java
+++ api/src/main/java/med/voll/api/entity/Paciente.java
@@ -12,6 +12,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedicos;
+import med.voll.api.paciente.DadosAtualizacaoPaciente;
 import med.voll.api.paciente.DadosCadastroPacientes;
 
 @Table(name = "pacientes")
@@ -45,5 +46,22 @@ public class Paciente {
 	    	this.cpf = dados.cpf();
 	    	this.endereco = new Endereco(dados.endereco());
 	    	
+	    }
+	    
+	    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+	        if (dados.nome() != null) {
+	            this.nome = dados.nome();
+	        }
+	        if (dados.telefone() != null) {
+	            this.telefone = dados.telefone();
+	        }
+	        if (dados.endereco() != null) {
+	            this.endereco.atualizarInformacoes(dados.endereco());
+	        }
+
+	    }
+
+	    public void excluir() {
+	        this.ativo = false;
 	    }
 }
