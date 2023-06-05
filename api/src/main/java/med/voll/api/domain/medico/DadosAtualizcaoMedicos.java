74225a0a09c611fa11a676ed75531cb9da569e82
Lucas Sales Salvo Petruci <99514230+LucasPetruci@users.noreply.github.com>
2023-06-04 23:07:32 -0300

    Update validações de médico

--- api/src/main/java/med/voll/api/domain/medico/DadosAtualizcaoMedicos.java
+++ api/src/main/java/med/voll/api/domain/medico/DadosAtualizcaoMedicos.java
@@ -1,13 +1,21 @@
 package med.voll.api.domain.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import med.voll.api.domain.endereco.DadosEndereco;
 
 public record DadosAtualizcaoMedicos(
 		@NotNull
 		Long id, 
+		
+		@NotBlank(message = "Nome obrigatório")
 		String nome, 
+		
+		@NotBlank(message = "Telefone obrigatório")
 		String telefone, 
-		DadosEndereco endereco ) {
+		
+	    @NotNull(message = "Dados do endereço são obrigatórios")
+		@Valid DadosEndereco endereco ) {
 
 }
