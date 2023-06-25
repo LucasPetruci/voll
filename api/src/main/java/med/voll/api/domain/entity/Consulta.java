a8c83c3970cf3dba6502f969795581c93a5cb1fb
Lucas Sales Salvo Petruci <99514230+LucasPetruci@users.noreply.github.com>
2023-06-25 20:29:41 -0300

    Create consulta
    Criado a funcionalidade de agendar consulta

---
+++ api/src/main/java/med/voll/api/domain/entity/Consulta.java
@@ -0,0 +1,36 @@
+package med.voll.api.domain.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "consultas")
+@Entity(name = "consulta")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Consulta {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "paciente_id")
+	private Medico medico;
+	
+	private LocalDateTime data;
+	
+}
