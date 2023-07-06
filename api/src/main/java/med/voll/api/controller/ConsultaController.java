b6baa9c634f658283ba56f97f1ba4b7f982c37f5
Lucas Sales Salvo Petruci <99514230+LucasPetruci@users.noreply.github.com>
2023-07-05 21:49:08 -0300

    add column motivo_cancelamento

--- api/src/main/java/med/voll/api/controller/ConsultaController.java
+++ api/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -2,6 +2,7 @@ package med.voll.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +28,8 @@ public class ConsultaController {
 		return ResponseEntity.ok(dto);
 	}
 	
+	@DeleteMapping
+	@Transactional
 	public ResponseEntity cancelar(@RequestBody @Valid DadosCancelamentoConsulta dados){
 		agenda.cancelar(dados);
 		return ResponseEntity.noContent().build();		
